37f0e834b623be64c56737a93e9892448eb2ed91
adaruiz <ada.ruiz93@gmail.com>
2022-04-04 17:32:51 -0700

    finished navbar

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+const navslide = () => {
+  const burger = document.querySelector(".burger-menu");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if(link.style.animation){
+        link.style.animation = ''
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 1.5}s`;
+      }
+    });
+    burger.classList.toggle('toggle');
+
+
+  });
+
+}
+
+
+
+navslide();
