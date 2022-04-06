5b09353c4e144ba9c2c1317e9b6c9324d0fe2de3
adaruiz <ada.ruiz93@gmail.com>
2022-04-05 17:03:43 -0700

    updated navbar

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const navslide = () => {
       if(link.style.animation){
         link.style.animation = ''
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 1.5}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.5}s`;
       }
     });
     burger.classList.toggle('toggle');
